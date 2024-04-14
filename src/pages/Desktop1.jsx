5d820b47a482513cae6ba91041198d8f110f8d40
Sneha Motghare <snehamotghare1127@gmail.com>
2024-04-14 09:02:02 +0530

    first commit

---
+++ src/pages/Desktop1.jsx
@@ -0,0 +1,283 @@
+import styles from "./Desktop1.module.css";
+
+
+const Desktop1 = () => {
+  return (
+    <div className={styles.desktop}>
+      <div className={styles.desktopChild} />
+      <div className={styles.stack}>
+        <div className={styles.bar}>
+          <div className={styles.thisPageIsIncludedInAFreParent}>
+            <div className={styles.thisPageIs}>
+              This page is included in a free SaaS Website Kit.
+            </div>
+            <div className={styles.viewTheCompleteKitParent}>
+              <div className={styles.viewTheComplete}>
+                View the complete Kit
+              </div>
+              <img className={styles.icons} alt="" src="/icons.svg" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.navBarDesktop}>
+          <div className={styles.logo}>
+            <div className={styles.logodefault}>
+              <div className={styles.logodefaultChild} />
+              <img
+                className={styles.logosaasIcon}
+                alt=""
+                src="/logosaas1@2x.png"
+              />
+            </div>
+            <div className={styles.madeBy}>made by</div>
+            <img
+              className={styles.blackHorizontal1Icon}
+              alt=""
+              src="/blackhorizontal-1.svg"
+            />
+          </div>
+          <div  className={styles.stack1  }>
+            <div className={styles.about}>About </div>
+            <div className={styles.features}>Features</div>
+            <div className={styles.customers}>Customers</div>
+            <div className={styles.updates}>Updates</div>
+            <div className={styles.help}>Help</div>
+            <div className={styles.buttons}>
+              <div className={styles.getForFree}>Get for free</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.hero}>
+          <div className={styles.rectangleParent}>
+            <div className={styles.groupChild} />
+            <div className={styles.groupItem} />
+            <div className={styles.heroContent}>
+              <div className={styles.tag}>
+                <div className={styles.version20IsHereParent}>
+                  <div className={styles.version20Is}>Version 2.0 is here</div>
+                  <div className={styles.readMoreParent}>
+                    <div className={styles.readMore}>Read more</div>
+                    <img className={styles.icons1} alt="" src="/icons1.svg" />
+                  </div>
+                </div>
+              </div>
+              <b className={styles.h1}>
+                <p className={styles.oneTask}>One Task</p>
+                <p className={styles.atATime}>at a Time</p>
+              </b>
+              <div className={styles.body}>
+                Celebrate the joy of accomplishment with an app designed to
+                track your progress, motivate your efforts, and celebrate your
+                successes.
+              </div>
+              <div className={styles.buttons1}>
+                <div className={styles.getForFree1}>Get for free</div>
+              </div>
+            </div>
+            <div className={styles.wrapperCursor1}>
+              <img
+                className={styles.cursor1Icon}
+                alt=""
+                src="/cursor-1@2x.png"
+              />
+            </div>
+            <img className={styles.cursor2Icon} alt="" src="/cursor-2@2x.png" />
+            <div className={styles.wrapperMessage2}>
+              <img
+                className={styles.message2Icon}
+                alt=""
+                src="/message-2@2x.png"
+              />
+            </div>
+            <img
+              className={styles.message1Icon}
+              alt=""
+              src="/message-1@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.logoTicker}>
+          <div className={styles.trustedByTheWorldsMostInParent}>
+            <div className={styles.trustedByThe}>
+              Trusted by the world’s most innovative teams
+            </div>
+            <div className={styles.acme1Parent}>
+              <img className={styles.acme1Icon} alt="" src="/acme-1@2x.png" />
+              <img
+                className={styles.quantum1Icon}
+                alt=""
+                src="/quantum-1@2x.png"
+              />
+              <img className={styles.echo1Icon} alt="" src="/echo-1@2x.png" />
+              <img
+                className={styles.celestia1Icon}
+                alt=""
+                src="/celestia-1@2x.png"
+              />
+              <img className={styles.pulse1Icon} alt="" src="/pulse-1@2x.png" />
+              <img className={styles.apex1Icon} alt="" src="/apex-1@2x.png" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.grid}>
+          <div className={styles.frameParent}>
+            <div className={styles.frameWrapper}>
+              <div className={styles.h2Parent}>
+                <b className={styles.h2}>Everything you need</b>
+                <div className={styles.body1}>
+                  Enjoy customizable lists, team work tools, and smart tracking
+                  all in one place. Set tasks, get reminders, and see your
+                  progress simply and quickly.
+                </div>
+              </div>
+            </div>
+            <div className={styles.featureCardsParent}>
+              <div className={styles.featureCards}>
+                <div className={styles.iconsWrapper}>
+                  <img className={styles.icons2} alt="" src="/icons2.svg" />
+                </div>
+                <div className={styles.integrationEcosystemParent}>
+                  <b className={styles.integrationEcosystem}>
+                    Integration ecosystem
+                  </b>
+                  <div className={styles.body2}>
+                    Enhance your productivity by connecting with your favorite
+                    tools, keeping all your essentials in one place.
+                  </div>
+                </div>
+              </div>
+              <div className={styles.featureCards1}>
+                <div className={styles.iconsContainer}>
+                  <img className={styles.icons3} alt="" src="/icons2.svg" />
+                </div>
+                <div className={styles.integrationEcosystemGroup}>
+                  <b className={styles.integrationEcosystem1}>
+                    Goal setting and tracking
+                  </b>
+                  <div className={styles.body3}>
+                    Define and track your goals, breaking down objectives into
+                    achievable tasks to keep your targets in sight.
+                  </div>
+                </div>
+              </div>
+              <div className={styles.featureCards2}>
+                <div className={styles.iconsFrame}>
+                  <img className={styles.icons4} alt="" src="/icons2.svg" />
+                </div>
+                <div className={styles.integrationEcosystemContainer}>
+                  <b className={styles.integrationEcosystem2}>
+                    Secure data encryption
+                  </b>
+                  <div className={styles.body4}>
+                    With end-to-end encryption, your data is securely stored and
+                    protected from unauthorized access.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.product}>
+          <div className={styles.frameGroup}>
+            <div className={styles.frameContainer}>
+              <div className={styles.h2Group}>
+                <b className={styles.h21}>Intuitive interface</b>
+                <div className={styles.body5}>
+                  Celebrate the joy of accomplishment with an app designed to
+                  track your progress, motivate your efforts, and celebrate your
+                  successes, one task at a time.
+                </div>
+              </div>
+            </div>
+            <div className={styles.wrapperAppNight}>
+              <img
+                className={styles.appNightIcon}
+                alt=""
+                src="/app-night@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.faq}>
+          <b className={styles.h22}>Frequently asked questions</b>
+          <div className={styles.faqs}>
+            <div className={styles.formfaq}>
+              <b className={styles.howDoesThe}>
+                How does the pricing work for teams
+              </b>
+              <div className={styles.frame}>
+                <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+              </div>
+            </div>
+            <div className={styles.formfaq1}>
+              <b className={styles.howDoesThe1}>
+                How does the pricing work for teams
+              </b>
+              <div className={styles.frame1}>
+                <img className={styles.vectorIcon1} alt="" src="/vector.svg" />
+              </div>
+            </div>
+            <div className={styles.formfaq2}>
+              <b className={styles.howDoesThe2}>
+                How does the pricing work for teams
+              </b>
+              <div className={styles.frame2}>
+                <img className={styles.vectorIcon2} alt="" src="/vector.svg" />
+              </div>
+            </div>
+            <div className={styles.formfaq3}>
+              <b className={styles.howDoesThe3}>
+                How does the pricing work for teams
+              </b>
+              <div className={styles.frame3}>
+                <img className={styles.vectorIcon3} alt="" src="/vector.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.signUp}>
+          <div className={styles.signUpInner}>
+            <div className={styles.frameDiv}>
+              <div className={styles.h2Container}>
+                <b className={styles.h23}>Get instant access</b>
+                <div className={styles.body6}>
+                  Celebrate the joy of accomplishment with an app designed to
+                  track your progress and motivate your efforts.
+                </div>
+              </div>
+              <div className={styles.form}>
+                <div className={styles.form1}>
+                  <div className={styles.nameemailcom}>name@email.com</div>
+                </div>
+                <div className={styles.buttons2}>
+                  <div className={styles.getForFree2}>Get access</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.emojistar1Icon}
+            alt=""
+            src="/emojistar-1@2x.png"
+          />
+          <img className={styles.helix21Icon} alt="" src="/helix2-1@2x.png" />
+        </div>
+        <div className={styles.footerS}>
+          <div className={styles.yourCompanyInc}>
+            @ 2024 Your Company, Inc. All rights reserved
+          </div>
+          <div className={styles.socialsParent}>
+            <img className={styles.socialsIcon} alt="" src="/socials.svg" />
+            <img className={styles.socialsIcon1} alt="" src="/socials1.svg" />
+            <img className={styles.socialsIcon2} alt="" src="/socials2.svg" />
+            <img className={styles.socialsIcon3} alt="" src="/socials3.svg" />
+            <img className={styles.socialsIcon4} alt="" src="/socials4.svg" />
+            <img className={styles.socialsIcon5} alt="" src="/socials5.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop1;
